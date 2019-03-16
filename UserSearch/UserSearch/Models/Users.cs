f44b634e5e68fcd85f8c80101966418c5dfb0538
HoyongJung <jhoyong57@gmail.com>
2019-03-16 17:48:38 -0600

    added a model

---
+++ UserSearch/UserSearch/Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace UserSearch.Models
+{
+    [Table("Users")]
+    public class Users
+    {
+        [Key]
+        public int userID { get; set; }
+
+        public string firstname { get; set; }
+
+        public string lastname { get; set; }
+
+        public string streetAddress { get; set; }
+
+        public int age { get; set; }
+
+        public string imgURL { get; set; }
+    }
+}
